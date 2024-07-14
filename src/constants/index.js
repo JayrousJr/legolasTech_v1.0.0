6dd570dac950cfdbd8d3a52a045577a890fc8a9a
Joshua Jayrous <joshuajayrous@gmail.com>
2024-07-14 17:10:25 +0300

    fixes

--- src/constants/index.js
+++ src/constants/index.js
@@ -78,25 +78,25 @@ export const feedback = [
 	{
 		id: "feedback-1",
 		content:
-			"Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-		name: "Herman Jensen",
+			"I just have nothing to say, but for a single word, i wil just say this is a game changer",
+		name: "Jayrous",
 		title: "Founder & Leader",
 		img: jayrous,
 	},
 	{
 		id: "feedback-2",
 		content:
-			"Money makes your life easier. If you're lucky to have it, you're lucky.",
-		name: "Steve Mark",
-		title: "Founder & Leader",
+			"I am seeing alot of profit right now, not like i used to run my business earlier",
+		name: "Ed Karta",
+		title: "Business Man",
 		img: edrick,
 	},
 	{
 		id: "feedback-3",
 		content:
-			"It is usually people in the money business, finance, and international trade that are really rich.",
-		name: "Kenn Gallagher",
-		title: "Founder & Leader",
+			"I think i am soon taking over the market in our city, the software are pro and i am safe",
+		name: "Lema Bry",
+		title: "COO",
 		img: bry,
 	},
 ];
